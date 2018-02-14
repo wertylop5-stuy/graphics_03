a2e4f078511e074aca37512d8ccd6f99e1d85f02
Stanley Lin <slin55055@gmail.com>
2018-02-14 11:10:42 -0500

    work

--- include/matrix.h
+++ include/matrix.h
@@ -13,4 +13,12 @@ void indent(struct matrix *m);
 
 void matrixMult(struct const matrix *a, struct matrix *b);
 
+void free_matrix(struct matrix *m);
+
+void resize(struct matrix *m);
+
+void grow_matrix();
+
+struct matrix* new_matrix();
+
 #endif
---
+++ matrix.c
@@ -0,0 +1,22 @@
+#include "include/matrix.h"
+
+void printMatrix(struct matrix *m) {
+	
+}
+
+void indent(struct matrix *m) {
+	
+}
+
+void matrixMult(struct const matrix *a, struct matrix *b) {
+	
+}
+
+void free_matrix(struct matrix *m) {
+	
+}
+
+void resize(struct matrix *m) {
+	
+}
+
