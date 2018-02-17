c79af8c26e7dce9467f127cea06e84077af73d37
Stanley Lin <slin55055@gmail.com>
2018-02-17 12:55:16 -0500

    fixed pointers

--- matrix.c
+++ matrix.c
@@ -7,11 +7,11 @@ struct matrix* new_matrix(int r, int c) {
 	
 	res->m = (float **)malloc(c*sizeof(float *));
 	
-	float *pos;
-	for (pos = *(res->m);
-			(pos - *(res->m))/sizeof(float *) < c;
-			pos += sizeof(float *)) {
-		pos = (float *)malloc(r*sizeof(float));
+	float **pos;
+	for (pos = res->m;
+			(pos - res->m) < c;
+			pos++ ) {
+		*pos = (float *)malloc(r*sizeof(float));
 	}
 	
 	return res;
@@ -21,12 +21,10 @@ void print_matrix(struct matrix *m) {
 	float *pos;
 	float **temp;
 	for (	temp = m->m;
-		(temp - m->m)/sizeof(float *) < m->rows;
-		temp += sizeof(float *)) {
-		printf("new row\n");
+		(temp - m->m) < m->rows;
+		temp++ ) {
 		
-		for (pos = *temp; (pos - *temp)/sizeof(float) < m->cols; pos += sizeof(float)) {
-			printf("uhh\n");
+		for (pos = *temp; (pos - *temp) < m->cols; pos++) {
 			printf("%f ", *pos);
 		}
 		printf("\n");
